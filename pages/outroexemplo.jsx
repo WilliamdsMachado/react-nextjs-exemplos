63598101cb082750a3497fb0b8a9231544a8908a
William Machado <dohillo@gmail.com>
2022-10-18 16:46:18 -0300

    Criado Header

---
+++ pages/outroexemplo.jsx
@@ -0,0 +1,17 @@
+import Header from '../src/components/header/Header'
+import Title from '../src/components/title/Title'
+import Subtitle from '../src/components/subtitle/Subtitle'
+
+
+function OutroExemploPage () {
+  return(
+    <>
+      <Header image={'https://sejadev-web-app-public-files.s3.amazonaws.com/courses/coffee2.jpg'}>
+        <Title>Café Dev</Title>
+        <Subtitle text ="O segundo mehor café"/>
+      </Header>
+      
+    </>
+  )
+}
+export default OutroExemploPage
